c449de32a4f4830cc7f27aa979c217b4f500beef
Srinjoy-Pramanik <pramaniksrinjoy5@gmail.com>
2025-05-10 23:39:56 +0530

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Projects from './components/Projects';
@@ -6,7 +6,7 @@ import Skills from './components/Skills';
 import About from './components/About';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-
+import Participation from './components/Participation';
 function App() {
   useEffect(() => {
     document.title = "SrinjoyPramanikSignatureCut";
@@ -16,6 +16,7 @@ function App() {
     <div className="bg-black text-white">
       <Navbar />
       <Home />
+      <Participation />
       <Projects />
       <Skills />
       <About />
